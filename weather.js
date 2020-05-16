38385e031f686180f4b66b4402d51e8ec02919d7
Brandon Humphry <brandonhumphry@gmail.com>
2020-05-15 19:31:41 -0700

    updating javascript file to pull second API

--- weather.js
+++ weather.js
@@ -1,67 +1,64 @@
 $(document).ready(function(){
 var city = null;
-var lat = '';
-var lon = '';
+var lat = ' ';
+var lon = ' ';
+var key = '&appid=ea3229d583947e23c0ad68c4233587d7&units=imperial';
 
-    $('#submitWeather').click(function(){
 
-        city = $("#city").val();
+$('#submitWeather').click(function(){
+    showWeather(city);
+    showForecast();
+});
 
-        if(city != ''){
+function showWeather(city){
+        city = $('#city').val();
+
+        if(city != ' '){
+            $('#error').html(' ');
 
             $.ajax({
-
-                url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + 
-                '&appid=ea3229d583947e23c0ad68c4233587d7&units=imperial',
+                url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + key,
                 type: 'GET',
-                dataType: 'json',
-                
+                dataType: 'json', 
+
                 success: function(data){
-                    console.log(data);
-                    
-                    $("#show").html(show(data));
-                    
-                    $("#city").val(' ');
-    
+                    console.log(data);  
+                    $('#show').html(show(data));                   
+                    $('#city').val(' ');   
                     lon = data.coord.lon;
                     lat = data.coord.lat;
-
-                $.ajax({
-                    url: 
-                    'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=ea3229d583947e23c0ad68c4233587d7',
-                    type: 'GET',
-                    dataType: 'json',
-
-                    success: function(data){
-                    console.log(data);
-                    $("#show").html(showForecast(data));
-                    }
-                });
-
-                }
-                
-            });
-                    
-
+                } 
+            });            
         } else {
+            $('#error').html('Field cannot be empty');}        
+    }
 
-            $("#error").html('Field cannot be empty');
-        
-        }
-            
+function showForecast(){
+    // $("#forecastDiv").attr();    
+    $.ajax({
+        url: 
+        'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + key,
+        type: 'GET',
+        dataType: 'json',
 
+    success: function(data2){
+    console.log(data2);
+    // $("#show").html(showForecast(data2));
+    }
     });
+} 
+
 
 });
 
+
 function show(data){
     return "<h2>" + data.name + "</h2>" +
-           "<h3><strong>Temp</strong>:"+ data.main.temp +"</h3>" +
+           "<h3><strong>Temp</strong>:"+ Math.floor(data.main.temp) +"</h3>" +
            "<h3><strong>Humidity</strong>:"+ data.main.humidity + "</h3>" +
-           "<h3><strong>Wind Speed</strong>:"+ data.wind.speed + "</h3>";
-        //    "<h2>" + data.current.uvi + "</h2>"; 
-}
+           "<h3><strong>Wind Speed</strong>:"+ data.wind.speed + "</h3>" +
+           "<h3><strong>lon</strong>:"+ data.coord.lon + "</h3>" +
+           "<h3><strong>lat</strong>:"+ data.coord.lat + "</h3>" +
+           "<img src="+'http://openweathermap.org/img/w/' + data.weather[0].icon + ".png>";
+};
 
-// function showForecast(data){
-//     return "<h2>" + data.current.uvi + "</h2>"; 
-// }
